3a5827553c01ca8fcb4e0752062d3b77b1dd5156
AjjlalAhmed <ajjlalahmed48@gmail.com>
2021-10-03 00:23:05 +0500

    Updating code

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "docbydev",
+    host: process.env.HOST,
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     charset: 'utf8mb4',
     collation: 'utf8mb4_unicode_ci',
 });
