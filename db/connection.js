654503877c1cda22bcfd943c4be407fc22f66e61
AjjlalAhmed <ajjlalahmed48@gmail.com>
2021-10-03 02:45:51 +0500

    Updating code

--- db/connection.js
+++ db/connection.js
@@ -3,12 +3,37 @@ const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER || "root",
     password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    charset: 'utf8mb4',
-    collation: 'utf8mb4_unicode_ci',
+    database: process.env.DATABASE || "docbydev",
+    charset: "utf8mb4",
+    collation: "utf8mb4_unicode_ci",
 });
 db.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    // connect to the MySQL server
+    let query = `CREATE TABLE IF NOT EXISTS users(   
+        id INT auto_increment, 
+        username VARCHAR(255),
+        useremail VARCHAR(1000),
+        userpassword VARCHAR(1000),
+        joined VARCHAR(1000),
+        profession VARCHAR(1000),
+        bio VARCHAR(1000),
+        skills VARCHAR(1000),
+        profileimage VARCHAR(1000),
+        phone TEXT,
+        address TEXT,
+        site TEXT,
+        birthday TEXT,
+        gender TEXT,
+        PRIMARY KEY(id)
+        )`;
+
+    db.query(query, function(err, results, fields) {
+        if (err) {
+            console.log(err);
+        }
+    });
+
 });
 module.exports = db;
